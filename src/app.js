8e3445487e6ab04778268a47c5a449738ede0f5d
Oscar Ballesta <oscarballestach@gmail.com>
2023-02-17 16:45:22 -0500

    Puerto

--- src/app.js
+++ src/app.js
@@ -13,11 +13,8 @@ app.use('/clientes', clienteRoutes);
 //app.use('/juegos', juegoRoutes);
 //app.use('/ventas', ventaRoutes);
 
-//app.listen(3000, () => {
-//  console.log('Servidor escuchando en el puerto 3000');
-//});
-
-const server = app.listen(0, () => {
-  const port = server.address().port;
-  console.log(`La aplicación está escuchando en el puerto ${port}!`);
+app.listen(3000, () => {
+ console.log('Servidor escuchando en el puerto 3000');
 });
+
+
